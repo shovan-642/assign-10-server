2027a188187d0322fbe874a9971ba8981d7e057b
shovan-642 <shovan642@gmail.com>
2025-03-05 22:18:02 +0600

    fav delete from db func complete

--- index.js
+++ index.js
@@ -94,6 +94,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/favoriteMovies/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await favoriteMovieCollection.deleteOne(query)
+        res.send(result)
+    })
+
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
